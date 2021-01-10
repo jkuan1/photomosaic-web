8c84a999fa428e64ae3d56777c13362eef0ffd50
jkuan1 <justinkuan2000@gmail.com>
2021-01-09 19:58:12 -0800

    added new components to the app and edited the main view of the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloFunctionComponent } from './hello-function/hello-function.component';
+import { ImageUploadComponent } from './image-upload/image-upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloFunctionComponent,
+    ImageUploadComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
