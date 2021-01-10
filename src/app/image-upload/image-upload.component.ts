6f1c5743a3f9ed6160fa6e864e6c0144c78283cd
jkuan1 <justinkuan2000@gmail.com>
2021-01-09 19:57:08 -0800

    created an image upload component to give the backend a template image to make a jpg out of

---
+++ src/app/image-upload/image-upload.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-image-upload',
+  templateUrl: './image-upload.component.html',
+  styleUrls: ['./image-upload.component.sass']
+})
+export class ImageUploadComponent{
+
+  constructor(private http: HttpClient) {}
+
+  selectedFile:File;
+  onFileSelected(event: any) {
+    
+    this.selectedFile = event.target.files[0]
+  }
+
+  onUpload() {
+    const fd = new FormData();
+    fd.append("image", this.selectedFile, this.selectedFile.name);
+    console.log(fd)
+    this.http.post("https://rm2251t9j3.execute-api.us-west-2.amazonaws.com/develop/mosaic", fd).subscribe(res => {console.log(res)})
+  }
+
+}
