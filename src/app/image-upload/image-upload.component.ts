1d957553266c33e32cef04b2a3daf51286d7ed41
jkuan1 <justinkuan2000@gmail.com>
2021-01-10 19:15:09 -0800

    personalized website and created an image preview window

--- src/app/image-upload/image-upload.component.ts
+++ src/app/image-upload/image-upload.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-image-upload',
@@ -8,9 +9,12 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ImageUploadComponent{
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+
+  }
 
   selectedFile:any;
+  
   // selectedFile:FormData = new FormData()
   onFileSelected(event: any) {   
     this.readThis(event.target)
@@ -18,16 +22,29 @@ export class ImageUploadComponent{
 
   onUpload() {
     console.log(this.selectedFile);
-    this.http.post("https://rm2251t9j3.execute-api.us-west-2.amazonaws.com/develop/mosaic", this.selectedFile).subscribe(res => {console.log(res)})
+
+    this.http.post("https://rm2251t9j3.execute-api.us-west-2.amazonaws.com/develop/mosaic", this.selectedFile).subscribe(res => {
+      console.log(res);
+      var image = new Image();
+      image.onload = function(){
+        console.log(image.width); // image is loaded and we have image width 
+      }
+      image.src = 'data:image/png;base64,'.concat(String(res));
+      (document.getElementById('userPic') as HTMLImageElement).replaceWith(image)
+    })
+    
+
   }
 
   readThis(inputValue: any): void {
     var file:File = inputValue.files[0];
     var myReader:FileReader = new FileReader();
   
-    myReader.onloadend = (e) => {
+    myReader.onloadend = (e:any) => {
       this.selectedFile = myReader.result;
+      (document.getElementById('userPic') as HTMLImageElement).src = e.target.result
     }
+
     myReader.readAsDataURL(file);
   }
 }
